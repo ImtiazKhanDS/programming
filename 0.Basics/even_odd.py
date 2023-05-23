f9122522ba8673ecf983668fbe0d9289e8732605
Imtiaz Khan <imtiazkhan@Imtiazs-MBP.bbrouter>
2023-05-23 15:01:52 +0530

    even or odd number using if construct

---
+++ 0.Basics/even_odd.py
@@ -0,0 +1,7 @@
+'''check if a variable is even or odd.'''
+if __name__=="__main__":
+    x=int(input("enter x value : "))
+    if x%2==0:
+        print(f"{x} is a even number")
+    else:
+        print(f"{x} is a odd number") 
